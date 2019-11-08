81c13a723d4ea7c74851fa14a966cc05c3c0d0e8
Eric Van Buren <edvanburen@gmail.com>
2019-11-07 19:40:39 -0500

    Bug Fixes

--- R/lr.twosigma.R
+++ R/lr.twosigma.R
@@ -1,8 +1,8 @@
 ##' lr.twosigma: Conveinent wrapper function for performing joint likelihood ratio tests in TWO-SIGMA
 ##' @param count Vector of non-negative integer read counts.
-##' @param mean_covar Covariates for the (conditional) mean model. Must be a matrix (without an intercept column) or = 1 to indicate an intercept only model.
-##' @param contrast Either a string indicating the column name of the covariate to test or an integer referring to its column position in BOTH the mean_covar and zi_covar matrices.
-##' @param zi_covar Covariates for the zero-inflation model. Must be a matrix (without an intercept column), = 1 to indicate an intercept only model, or = 0 to indicate no zero-inflation model desired.
+##' @param mean_covar Covariates for the (conditional) mean model. Must be a matrix (without an intercept column) or a vector if a single covariate is being tested.
+##' @param zi_covar Covariates for the zero-inflation model. Must be a matrix (without an intercept column) or a vector if a single covariate is being tested.
+##' @param contrast Either a string indicating the column name of the covariate to test or an integer referring to its column position in BOTH the mean_covar and zi_covar matrices (if the two matrices differ using a string name is preferred). Argument is ignored if mean_covar and zi_covar are both a single covariate (as that covariate is assumed of interest).
 ##' @param mean_re Should random intercepts be included in the (conditional) mean model?
 ##' @param zi_re Should random intercepts be included in the zero-inflation model?
 ##' @param id Vector of individual-level ID's. Used for random effect prediction.
@@ -45,8 +45,11 @@ lr.twosigma<-function(count,mean_covar,zi_covar,contrast#,joint=TRUE
   }
 
   if(is.character(contrast)){
-    if(!(contrast%in%colnames(mean_covar) & contrast%in%colnames(zi_covar))){
-      stop("contrast not found in both matrices")
+      if(!is.atomic(mean_covar)&!is.atomic(zi_covar))
+      {
+        if(!(contrast%in%colnames(mean_covar) & contrast%in%colnames(zi_covar))){
+          stop("contrast not found in both matrices")
+      }
     }
   }
   fit_alt<-glmmTMB(formula=formulas$mean_form
@@ -59,19 +62,21 @@ lr.twosigma<-function(count,mean_covar,zi_covar,contrast#,joint=TRUE
   # need also to point out that users have some responsibilities here
 
   if(is.numeric(contrast)){
-    if(contrast>max(ncol(get("mean_covar")),ncol(get("zi_covar")))){
-      stop("Contrast seems to be ill-defined")
+    if(!is.atomic(mean_covar)&!is.atomic(zi_covar)){
+      if(contrast>max(ncol(mean_covar),ncol(zi_covar))){
+        stop("Contrast seems to be ill-defined")
+      }
     }
     if(is.atomic(mean_covar) & is.atomic(zi_covar)){
-      formulas$mean_form<-as.formula(gsub("mean_covar","",format(formulas$mean_form)))
-      formulas$zi_form<-as.formula(gsub("zi_covar","",format(formulas$zi_form)))
+      formulas$mean_form<-as.formula(gsub("mean_covar","1",format(formulas$mean_form)))
+      formulas$zi_form<-as.formula(gsub("zi_covar","1",format(formulas$zi_form)))
     }
     if(!is.atomic(mean_covar) & is.atomic(zi_covar)){
       formulas$mean_form<-as.formula(gsub("mean_covar","mean_covar[,-contrast]",format(formulas$mean_form)))
-      formulas$zi_form<-as.formula(gsub("zi_covar","",format(formulas$zi_form)))
+      formulas$zi_form<-as.formula(gsub("zi_covar","1",format(formulas$zi_form)))
     }
     if(is.atomic(mean_covar) & !is.atomic(zi_covar)){
-      formulas$mean_form<-as.formula(gsub("mean_covar","",format(formulas$mean_form)))
+      formulas$mean_form<-as.formula(gsub("mean_covar","1",format(formulas$mean_form)))
       formulas$zi_form<-as.formula(gsub("zi_covar","zi_covar[,-contrast]",format(formulas$zi_form)))
     }
     if(!is.atomic(mean_covar) & !is.atomic(zi_covar)){
@@ -79,16 +84,26 @@ lr.twosigma<-function(count,mean_covar,zi_covar,contrast#,joint=TRUE
       formulas$zi_form<-as.formula(gsub("zi_covar","zi_covar[,-contrast]",format(formulas$zi_form)))
     }
   }else{
-    index<-which(colnames(mean_covar)==contrast)
-    if(length(index)==0){
-      stop("Contrast Name not found in colnames of mean model covariate data matrix")
+    if(!is.atomic(mean_covar)){
+      index<-which(colnames(mean_covar)==contrast)
+      if(length(index)==0){
+        stop("Contrast Name not found in colnames of mean model covariate data matrix")
+      }
+      formulas$mean_form<-as.formula(gsub("mean_covar","mean_covar[,-index]",format(formulas$mean_form)))
     }
-    formulas$mean_form<-as.formula(gsub("mean_covar","mean_covar[,-index]",format(formulas$mean_form)))
+    if(is.atomic(mean_covar)){
+      formulas$mean_form<-as.formula(gsub("mean_covar","1",format(formulas$mean_form)))
+    }
+    if(!is.atomic(zi_covar)){
     index<-which(colnames(zi_covar)==contrast)
     if(is.null(index)){
       stop("Contrast Name not found in colnames of zi model covariate data matrix")
     }
     formulas$zi_form<-as.formula(gsub("zi_covar","zi_covar[,-index]",format(formulas$zi_form)))
+    }
+    if(is.atomic(zi_covar)){
+      formulas$zi_form<-as.formula(gsub("zi_covar","1",format(formulas$zi_form)))
+    }
   }
 
 fit_null<-glmmTMB(formula=formulas$mean_form
