87c8b0ab11a03290b52d74e27ec0d814f8041ca4
Eric Van Buren <edvanburen@gmail.com>
2020-06-15 16:11:11 -0400

    Temp

--- R/twosigmag4.R
+++ R/twosigmag4.R
@@ -385,7 +385,9 @@ twosigmag4<-function(count_matrix,index_test,index_ref=NULL,all_as_ref=FALSE,mea
   #a<-pbapply(count_matrix[genes,],MARGIN=1,FUN=fit_tsg,statistic=statistic,
  #     covar_to_test=covar_to_test,factor_name=factor_name,contrast_matrix=contrast_matrix,
   #     id=id,cl=cl)
-  a<-pblapply(X=genes,FUN=fit_tsg2,cl=cl)
+  a<-pblapply(X=genes,FUN=fit_tsg2,statistic=statistic,
+         covar_to_test=covar_to_test,factor_name=factor_name,contrast_matrix=contrast_matrix,
+         id=id,cl=cl)
   if(ncores>1){parallel::stopCluster(cl)}
   rm(count_matrix)
   # a<-bplapply(genes,FUN=fit_tsg2,statistic=statistic,
